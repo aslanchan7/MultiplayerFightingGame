c6c23c189ff06900081e253484d937f77a48d5fc
aslanchan7 <124756060+aslanchan7@users.noreply.github.com>
2024-08-08 17:10:18 +0800

    Implemented temporary art

--- MultiplayerFightingGame/Assets/Scripts/PlayerController.cs
+++ MultiplayerFightingGame/Assets/Scripts/PlayerController.cs
@@ -1,50 +1,153 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.MPE;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movements")]
-    [SerializeField] KeyCode moveLeftKey = KeyCode.A;
-    [SerializeField] KeyCode moveRightKey = KeyCode.D;
-    [SerializeField] KeyCode jumpKey = KeyCode.W;
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] LayerMask groundLayer;
     private bool onGround;
+    float moveX = 0f;
+
+    // Jumping
+    private float lastJump;
+    private float jumpCooldown = 0.2f;
+
+    // Crouching
+    private bool crouching;
 
     // References
     private Rigidbody2D rb;
+    private Vector3 initialScale;
+    private PlayerControls playerControls;
+    private Animator animator;
+    private string currentAnimState;
+
+    // Animation States
+    const string PLAYER_IDLE = "PlayerIdle";
+    const string PLAYER_JUMP_UP = "PlayerJumpUp";
+    const string PLAYER_JUMP_DOWN = "PlayerJumpDown";
+    const string PLAYER_WALK = "PlayerWalk";
+
+    void Awake()
+    {
+        playerControls = new PlayerControls();
+        playerControls.Player.Enable();
+        playerControls.Player.Jump.performed += Jump;
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        initialScale = transform.localScale;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float moveX = 0f;
-
         // Left & Right Movements
-        if (Input.GetKey(moveRightKey))
+        moveX = 0f;
+        if (playerControls.Player.MoveHorizontal.ReadValue<float>() > 0f)
         {
             moveX += 1f;
         }
 
-        if (Input.GetKey(moveLeftKey))
+        if (playerControls.Player.MoveHorizontal.ReadValue<float>() < 0f)
         {
-            moveX += -1f;
+            moveX -= 1f;
         }
 
-        // Jumping
+        // Crouching
         float distToGround = GetComponent<Collider2D>().bounds.extents.y;
         onGround = Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.1f, groundLayer);
 
-        if (Input.GetKeyDown(jumpKey) && onGround)
+        if (playerControls.Player.Crouch.ReadValue<float>() == 1f && onGround)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            // Play Crouch Animation / State
+            crouching = true;
+        }
+        else if (playerControls.Player.Crouch.ReadValue<float>() == 0f)
+        {
+            crouching = false;
         }
 
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        // Manage Crouching Animations
+        if (crouching)
+        {
+            Crouch();
+            if (Mathf.Abs(playerControls.Player.MoveHorizontal.ReadValue<float>()) > 0.7f || rb.velocity.y > 0.1f)
+            {
+                UnCrouch();
+            }
+        }
+        else
+        {
+            UnCrouch();
+        }
+
+        // Manage Idle & Walking Animations
+        if (onGround && Mathf.Abs(rb.velocity.y) < 0.1f)
+        {
+            if (moveX == 0)
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_WALK);
+            }
+        }
+
+        // Manage Jump Animations
+        if (!onGround && rb.velocity.y < 0)
+        {
+            ChangeAnimationState(PLAYER_JUMP_DOWN);
+        }
+
+        if (!crouching)
+        {
+            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        float distToGround = GetComponent<Collider2D>().bounds.extents.y;
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.1f, groundLayer);
+        if (onGround && Time.time - lastJump > jumpCooldown)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            lastJump = Time.time;
+            ChangeAnimationState(PLAYER_JUMP_UP);
+            onGround = false;
+        }
+    }
+
+    private void Crouch()
+    {
+        // ChangeAnimationState(PLAYER_CROUCH);
+        transform.localScale = new Vector3(initialScale.x, 0.5f * initialScale.y, initialScale.z);
+        moveX = 0f;
+    }
+
+    private void UnCrouch()
+    {
+        // Check if moving then ChangeAnimationState(PLAYER_MOVE)
+        // Or check if not moving then ChangeAnimationState(Player_IDLE)
+        crouching = false;
+        transform.localScale = initialScale;
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentAnimState == newState) return;
+
+        animator.Play(newState);
+
+        currentAnimState = newState;
     }
 }
